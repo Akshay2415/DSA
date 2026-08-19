b2b1793fe82cc3f401081c91e44b87180599c21e
Akshay2415 <akshaygirnare1234@gmail.com>
2026-08-19 15:53:51 +0530

    Leet problems

---
+++ Leetcode/Leet_283.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = -1;
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+//runtime error happens if there is no zero in array so 
+    if(j == -1){
+        return;
+    }
+
+        for(int i = j+1; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i] ,nums[j]);
+                j++;
+            }
+        }
+    }
+};
