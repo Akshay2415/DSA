bb94a3ced0c96e7360bc44cb070103268e96bc59
Akshay2415 <akshaygirnare1234@gmail.com>
2026-08-21 23:14:17 +0530

    minor change

--- Arrays/longest_subarr.cpp
+++ Arrays/longest_subarr.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+//long long is use because if value is something
+//greater then overtime error might happen thats why 
 int logestSubarr(vector<int>a, long long k){
     map<long long,int>prefixSum;
     long long sum = 0;
@@ -18,6 +21,8 @@ int logestSubarr(vector<int>a, long long k){
             maxLen = max(maxLen, len);
         }
         
+        //this codition is for if there is zero's 
+        //in array that whys
         if(prefixSum.find(sum) == prefixSum.end()){
             prefixSum[sum]= i;
         }
